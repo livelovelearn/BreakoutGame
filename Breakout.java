4e30d447178b66b442d87125ba0bdddea555d952
Yancheng Liu <morningsherry@hotmail.com>
2013-10-27 16:29:51 -0400

    Update Breakout.java

--- Breakout.java
+++ Breakout.java
@@ -1,10 +1,5 @@
 /*
- * File: Breakout.java
- * -------------------
- * Name:
- * Section Leader:
- * 
- * This file will eventually implement the game of Breakout.
+ * This file implements the game of Breakout.
  */
 
 import acm.graphics.*;
@@ -70,38 +65,39 @@ public class Breakout extends GraphicsProgram {
 		setupPaddle();
 		
 		setupBall();
-		waitForClick();
-		vx = rgen.nextDouble (1.0, 3.0);
-		if (rgen.nextBoolean(0.5))
+		waitForClick(); // wait for players to start the game
+		vx = rgen.nextDouble (1.0, 3.0); //set a random initiation velocity for the ball
+		if (rgen.nextBoolean(0.5)) // random moving direction
 			vx=-vx;
 		
 		while (true)
 		{
 			moveBall();
 			GObject collider = getCollidingObject();
-			if (collider == paddle)//can use == here?
+			if (collider == paddle)//use == to test whether the ball hits the paddle
 			{
-				vy=-vy;
+				vy=-vy; // then start to move upwards
 			}
-			else if (collider != null)
+			else if (collider != null) 
+			/* if the ball hits some object other a paddle, it must hit a brick */
 			{
-				vy=-vy;
-				remove(collider);
+				vy=-vy; // again the ball changes it direction
+				remove(collider); // while the collided brick is remove!
 			}
 			if ((ball.getX() > WIDTH-2*BALL_RADIUS)||ball.getX() <0)
 			{
-				vx=-vx;
+				vx=-vx; // if the ball hit the left or right wall, it will bounce back
 			}
 			if (ball.getY()<0)
 			{
-				vy=-vy;
+				vy=-vy; // if the ball hit the ceiling, it will bounce back
 			}
-			if (ball.getY() > HEIGHT-2*BALL_RADIUS)
+			if (ball.getY() > HEIGHT-2*BALL_RADIUS) // if the ball fall off the bottom
 			{
-				if(chance!=0)
+				if(chance!=0) // if the player has any more chance to start over
 					{chance--;
 					setupBall();
-					waitForClick();
+					waitForClick();// then start over
 
 					vx = rgen.nextDouble (1.0, 3.0);
 					if (rgen.nextBoolean(0.5))
@@ -109,6 +105,7 @@ public class Breakout extends GraphicsProgram {
 					}
 					
 				else
+				/* if the player does not have any more chance, then show "You lose" and end the game */
 					add(new GLabel("You lose", WIDTH/2, HEIGHT/2));
 			}
 			
@@ -125,7 +122,7 @@ public class Breakout extends GraphicsProgram {
 		paddle.setLocation(e.getX(), e.getY());
 	}
 
-	private void setupWall() 
+	private void setupWall() // to build up colorful brick layers
 	{
 		for (int j=0; j<NBRICK_ROWS; j++) //build multiple levels
 		{
@@ -134,7 +131,7 @@ public class Breakout extends GraphicsProgram {
 			int xcord = (BRICK_WIDTH + BRICK_SEP) * i;
 			int ycord = BRICK_Y_OFFSET + (BRICK_HEIGHT + BRICK_SEP)* j;
 			GRect rect = new GRect(xcord, ycord, BRICK_WIDTH, BRICK_HEIGHT);
-			rect.setFilled(true);
+			rect.setFilled(true); // filled with color
 			switch (j%10){
 						case 0: rect.setFillColor(Color.RED);
 						break;
@@ -157,19 +154,19 @@ public class Breakout extends GraphicsProgram {
 						case 9: rect.setFillColor(Color.CYAN);
 						break;
 						}
-		    add(rect);
+		    add(rect); // add to the canvas
 		   }
 		}
 	}	
 	
-	private void setupPaddle()
+	private void setupPaddle() // draw the paddle
 	{
 		paddle = new GRect(0, APPLICATION_HEIGHT-PADDLE_Y_OFFSET-PADDLE_HEIGHT, PADDLE_WIDTH, PADDLE_HEIGHT);
 		paddle.setFilled(true);
 		add(paddle);
 	}
 	
-	private void setupBall()
+	private void setupBall() // draw the ball
 	{
 		ball = new GOval(WIDTH/2-BALL_RADIUS/2, HEIGHT/2+BALL_RADIUS/2, BALL_RADIUS, BALL_RADIUS);
 		ball.setFilled(true);
@@ -180,6 +177,9 @@ public class Breakout extends GraphicsProgram {
 		ball.move(vx, vy);
 	}
 	
+	/*check whether the ball collides with any object:
+	remember the ball is a circle instead of one point,
+	so the program need to check four farthest points*/
 	private GObject getCollidingObject()
 	{
 		GObject ob = null;
